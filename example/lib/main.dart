bb202335a37e93f70dfcb5656a5686f216b14219
Airyzz <36567925+Airyzz@users.noreply.github.com>
2024-07-14 17:21:53 +0930

    initial windows support

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -48,7 +48,7 @@ class _MyAppState extends State<MyApp> {
                   child: Text("Start")),
               TextButton(
                   onPressed: () {
-                    constellation_dart.setDisplay("eDP-1");
+                    constellation_dart.setWindow("67424");
                     constellation_dart.createCursor(
                         "id_1", "Hello from flutter!");
                     constellation_dart.setCursorColor("id_1", Colors.amber);
